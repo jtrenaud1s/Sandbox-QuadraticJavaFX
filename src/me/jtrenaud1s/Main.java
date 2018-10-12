0195e3908652491ef9bde29d59d2ba008766d7cc
BuildTools <unconfigured@null.spigotmc.org>
2018-10-12 15:54:21 -0500

    Quadratic Formula UI

--- src/me/jtrenaud1s/Main.java
+++ src/me/jtrenaud1s/Main.java
@@ -20,34 +20,43 @@ public class Main extends Application {
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(5);
         grid.setHgap(5);
-//Defining the Name text field
-        final TextField aText = new TextField();
+
+
+        TextField aText = new TextField();
         aText.setPromptText("Enter the value of a: ");
         aText.setPrefColumnCount(10);
         aText.getText();
         GridPane.setConstraints(aText, 0, 0);
+        GridPane.setColumnSpan(aText, 2);
         grid.getChildren().add(aText);
-//Defining the Last Name text field
-        final TextField bText = new TextField();
+
+        TextField bText = new TextField();
         bText.setPromptText("Enter the value of b: ");
         GridPane.setConstraints(bText, 0, 1);
+        GridPane.setColumnSpan(bText, 2);
         grid.getChildren().add(bText);
-//Defining the Comment text field
-        final TextField cText = new TextField();
+
+        TextField cText = new TextField();
         cText.setPrefColumnCount(15);
         cText.setPromptText("Enter the value of c: ");
         GridPane.setConstraints(cText, 0, 2);
+        GridPane.setColumnSpan(cText, 2);
         grid.getChildren().add(cText);
-//Defining the Submit button
+
         Button submit = new Button("Calculate");
-        GridPane.setConstraints(submit, 1, 0);
+        GridPane.setConstraints(submit, 0, 3);
+        GridPane.setFillWidth(submit, true);
         grid.getChildren().add(submit);
-//Defining the Clear button
+
         Button clear = new Button("Clear");
-        GridPane.setConstraints(clear, 1, 1);
+        GridPane.setConstraints(clear, 1, 3);
+        GridPane.setFillWidth(clear, true);
         grid.getChildren().add(clear);
-        Scene scene = new Scene(grid, 300, 200);
+
+        Scene scene = new Scene(grid);
+
         primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
         primaryStage.show();
 
         submit.setOnAction(new EventHandler<ActionEvent>() {
